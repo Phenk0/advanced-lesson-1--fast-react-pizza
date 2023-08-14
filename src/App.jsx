0d3da4b89a0fc6e1db31de7dce040d70bdc5c323
Roman Parkhomenko <roman.s.parkhomenko@gmail.com>
2023-08-14 18:25:54 +0300

    part 6: router 6.4+

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home.jsx";
+import Menu from "./feature/menu/Menu.jsx";
+import Cart from "./feature/cart/Cart.jsx";
+import Order from "./feature/order/Order.jsx";
+import CreateOrder from "./feature/order/CreateOrder.jsx";
+import Error from "./ui/Error.jsx";
+
 function App() {
-  return <>APP</>;
+  const router = createBrowserRouter([
+    { path: "/", Component: Home, errorElement: <Error /> },
+    { path: "/menu", Component: Menu },
+    { path: "/cart", Component: Cart },
+    { path: "/order/new", Component: CreateOrder },
+    { path: "/order/:orderId", Component: Order },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
